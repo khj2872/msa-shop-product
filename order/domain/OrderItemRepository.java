2898f09ed2b457598c587201744a673753648f1b
kang <hykang2872@gmail.com>
2021-04-01 13:32:38 +0900

    불필요 파일 제거

--- order/domain/OrderItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.msa.shop.order.domain;//package com.msa.shop.order.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderLine, Long> {
-}
--- order/domain/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.msa.shop.order.domain;//package com.msa.shop.order.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, OrderId> {
-}
