bc7c65fa31a28ff01cf97104562582a4f54953ff
Hyeokjin <gurwls1060@naver.com>
2021-04-13 22:04:34 +0900

    sample data script 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO CATEGORY VALUES ('1', now(), now(), '식품', null);
+INSERT INTO CATEGORY VALUES ('2', now(), now(), '과일', '1');
+INSERT INTO CATEGORY VALUES ('3', now(), now(), '채소', '1');
+INSERT INTO CATEGORY VALUES ('4', now(), now(), '수산물', '1');
+
+INSERT INTO CATEGORY VALUES ('5', now(), now(), '주방용품', null);
+INSERT INTO CATEGORY VALUES ('6', now(), now(), '냄비', '5');
+INSERT INTO CATEGORY VALUES ('7', now(), now(), '칼', '5');
+
+INSERT INTO CATEGORY VALUES ('8', now(), now(), '가전디지털', null);
+INSERT INTO CATEGORY VALUES ('9', now(), now(), '냉장고', '8');
+INSERT INTO CATEGORY VALUES ('10', now(), now(), '세탁기', '8');
+INSERT INTO CATEGORY VALUES ('11', now(), now(), '노트북', '8');
